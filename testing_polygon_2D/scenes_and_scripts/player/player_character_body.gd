671a125553bdb5a75ed47c268fab9b07186ec3ba
darktryer <134670587+darktryer@users.noreply.github.com>
2024-02-10 23:33:43 +1000

    Add files via upload

---
+++ testing_polygon_2D/scenes_and_scripts/player/player_character_body.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	var velocity = Vector2()
+	
+	if Input.is_action_pressed('ui_right'):
+		velocity.x += 1
+	if Input.is_action_pressed('ui_left'):
+		velocity.x -= 1
+	if Input.is_action_pressed('ui_down'):
+		velocity.y += 1
+	if Input.is_action_pressed('ui_up'):
+		velocity.y -= 1
+	return move_and_slide() #velocity * SPEED
